08d0e4c234724c9b8675c53aa036da57d1486c28
Willyan Patrykc <96625051+WILLY4N@users.noreply.github.com>
2023-09-14 14:02:07 -0300

    Implementação Classe Ponto, atributos e métodos.

--- app/src/main/java/br/com/bitan/clickponto/model/Ponto.java
+++ app/src/main/java/br/com/bitan/clickponto/model/Ponto.java
@@ -1,4 +1,54 @@
 package br.com.bitan.clickponto.model;
 
+import android.text.format.Time;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Ponto {
+    //atributos
+    //https://stackoverflow.com/questions/20326619/time-in-hhmm-format
+    private int jornada=480;
+    private LocalTime entrada1;
+    private LocalTime saida1;
+    private LocalTime entrada2;
+
+    public LocalTime getEntrada1() {
+        return entrada1;
+    }
+
+    public void setEntrada1(LocalTime entrada1) {
+        this.entrada1 = entrada1;
+    }
+
+    public LocalTime getSaida1() {
+        return saida1;
+    }
+
+    public void setSaida1(LocalTime saida1) {
+        this.saida1 = saida1;
+    }
+
+    public LocalTime getEntrada2() {
+        return entrada2;
+    }
+
+    public void setEntrada2(LocalTime entrada2) {
+        this.entrada2 = entrada2;
+    }
+
+    public LocalTime getSaida2() {
+        return saida2;
+    }
+
+    public void setSaida2(LocalTime saida2) {
+        this.saida2 = saida2;
+    }
+
+    private LocalTime saida2;
+
+        //Construtor
+    public Ponto(){
+
+    }
 }
--- app/src/main/java/br/com/bitan/clickponto/view/MainActivity.java
+++ app/src/main/java/br/com/bitan/clickponto/view/MainActivity.java
@@ -3,14 +3,48 @@ package br.com.bitan.clickponto.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 import br.com.bitan.clickponto.R;
+import br.com.bitan.clickponto.model.Ponto;
 
 public class MainActivity extends AppCompatActivity {
+
+    Ponto ponto;
+
+    EditText editTimeEntrada1;
+    EditText editTimeSaida1;
+    EditText editTimeEntrada2;
+    EditText editTimeSaida2;
+
+    Button btnLimpar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ponto = new Ponto();
+        ponto.setEntrada1(LocalTime.parse("08:32"));
+        ponto.setSaida1(LocalTime.parse("13:06"));
+        ponto.setEntrada2(LocalTime.parse("14:12"));
+        ponto.setSaida2(ponto.getEntrada2().plusMinutes(480-ponto.getEntrada1().until(ponto.getSaida1(), ChronoUnit.MINUTES)));
+
+        editTimeEntrada1 = findViewById(R.id.editTimeEntrada1);
+        editTimeEntrada1 = findViewById(R.id.editTimeSaida1);
+        editTimeEntrada1 = findViewById(R.id.editTimeEntrada2);
+        editTimeEntrada1 = findViewById(R.id.editTimeSaida2);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+
+        editTimeEntrada1.setText(ponto.getEntrada1());
+        editTimeEntrada1.setText(ponto.getSaida1());
+        editTimeEntrada1.setText(ponto.getEntrada2());
+        editTimeEntrada1.setText(ponto.getSaida2());
+
     }
 }
